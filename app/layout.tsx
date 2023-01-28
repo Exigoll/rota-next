fa9915bf873fae6c32ca7413ae84ca699ce52273
Exigoll <exigoll@bk.ru>
2023-01-28 15:51:56 +0500

    fix: type in Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./styles/globals.scss";
 import { useState } from "react";
 
 interface RootLayoutProps {
-	children: React.ReactElement;
+	children: React.ReactNode;
 }
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
